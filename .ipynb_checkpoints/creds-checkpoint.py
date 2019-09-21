d930e88349059e857f1756fdc643844555689fb3
Jamie Boyle <jamie.boyle@moneysmart.com>
2019-09-21 15:57:11 +0000

    Athena queries now to source table and with partitions, but not returning results to sagemaker (query executes correctly)

---
+++ .ipynb_checkpoints/creds-checkpoint.py
@@ -0,0 +1,4 @@
+# All this should be added into secrets manager, and kept out of github
+
+scalyr_api_key = "05rJlfheobFneIllyD3PNmvqN9MOMFdfuhxfS56DOW4I-"
+pg_pass = "BVHSjgnqHpqPRgHHaG9sYdJqD94ZeykJ"
